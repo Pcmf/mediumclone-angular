5fdb429a2039cbee289d9f5b86d83e482a65c57f
Pcmf <pedroferreira2005@gmail.com>
2023-05-31 23:15:20 +0100

    Pagination added. Installed 'query-string' (--force)

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -4,18 +4,30 @@ import { feedActions } from "./store/actions";
 import { combineLatest } from "rxjs";
 import { selectError, selectFeedData, selectIsLoading } from "./store/reducers";
 import { CommonModule } from "@angular/common";
-import { RouterLink } from "@angular/router";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { ErrorMessageComponent } from "../error-message/error-message.component";
 import { IsLoadingComponent } from "../is-loading/is-loading.component";
+import { environment } from "src/environments/environment.development";
+import { PaginationComponent } from "../pagination/pagination.component";
+import queryString from "query-string";
 
 @Component({
     selector: 'mc-feed',
     templateUrl: './feed.component.html',
     standalone: true,
-    imports: [CommonModule, RouterLink, ErrorMessageComponent, IsLoadingComponent]
+    imports: [
+        CommonModule, 
+        RouterLink, 
+        ErrorMessageComponent, 
+        IsLoadingComponent,
+        PaginationComponent,
+    ]
 })
 export class FeedComponent implements OnInit {
     @Input() apiUrl:string;
+    limit = environment.limit;
+    baseUrl = this.router.url.split('?')[0];
+    currentPage: number = 0;
 
     data$ = combineLatest({
         isLoading: this.store.select(selectIsLoading),
@@ -23,9 +35,30 @@ export class FeedComponent implements OnInit {
         feed: this.store.select(selectFeedData)
     })
 
-    constructor(private store: Store) {}
+    constructor(
+        private store: Store, 
+        private router: Router,
+        private route: ActivatedRoute,
+    ) {}
 
     ngOnInit() {
-        this.store.dispatch(feedActions.getFeed({url: this.apiUrl}))
+        this.fetchFeed();
+
+        this.route.queryParams.subscribe(params => {
+            this.currentPage = Number(params['page'] || 1);
+            this.fetchFeed();
+        })
+    }
+
+    fetchFeed(): void{
+        const offset = this.currentPage * this.limit - this.limit;
+        const parseUrl = queryString.parseUrl(this.apiUrl);
+        const stringifiedParams = queryString.stringify({
+            limit: this.limit,
+            offset,
+            ...parseUrl.query,
+        });
+        const apiUrlWithParams = `${parseUrl.url}?${stringifiedParams}`;
+        this.store.dispatch(feedActions.getFeed({url: apiUrlWithParams}));
     }
 }
